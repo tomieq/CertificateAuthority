631e25dbdf772be1b6fe026fd1be0bce6451df9e
tomieq <tomieq@wp.pl>
2026-04-23 13:09:01 +0200

    Added nice padding to description

--- Sources/CertificateAuthority/X509Certificate+String.swift
+++ Sources/CertificateAuthority/X509Certificate+String.swift
@@ -4,19 +4,20 @@
 // 
 //  Created by: tomieq on 23/04/2026
 //
+import SwiftExtensions
 
 extension X509Certificate: CustomStringConvertible {
     public var description: String {
         var output = "Serial number:\n\t\(serialNumber.hexString)"
-        output.append("\nIssuer:\n\t\(issuer)")
-        output.append("\nValidity:\n\t\(validity)")
-        output.append("\nSubject:\n\t\(subject)")
+        output.append("\nIssuer:\n\t\(issuer.description.replacingOccurrences(of: ", ", with: "\n\t"))")
+        output.append("\nValidity:\n\t\(validity.description.replacingOccurrences(of: ", ", with: "\n\t"))")
+        output.append("\nSubject:\n\t\(subject.description.replacingOccurrences(of: ", ", with: "\n\t"))")
         output.append("\nPublic Key:\n\t\(publicKey)")
         output.append("\nExtensions:\n\t\(extensions.count)")
         for ext in self.extensions {
             output.append("\n\(ext)")
         }
-        return output
+        return "X509Certificate {\n\t\(output.replacingOccurrences(of: "\n", with: "\n\t"))\n}"
     }
 }
 
